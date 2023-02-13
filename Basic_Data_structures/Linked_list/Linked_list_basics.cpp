c7c94ad8caed87be8e4c33cb4e7f57b82c3101b3
Chandrakant <83269908+nightfury000@users.noreply.github.com>
2023-02-13 21:49:54 +0530

    sword_question

---
+++ Basic_Data_structures/Linked_list/Linked_list_basics.cpp
@@ -0,0 +1,40 @@
+/*
+A linked list is a linear data structure, in which the elements are 
+not stored at contiguous(sharing common border) memory locations. 
+The elements in a linked list are linked using pointers.
+
+         Head
+        /
+       /
+      /
+     /
+    /
+┌──────────┬──────┐        ┌──────────┬──────┐        ┌──────────┬──────┐        ┌──────────┬──────┐
+│          │      │        │          │      │        │          │      │        │          │      │
+│          │      ├───────►│          │      ├───────►│          │      ├───────►│          │      ├───────► NULL
+│   A      │      │        │    B     │      │        │    C     │      │        │    D     │      │
+│          │      │        │          │      │        │          │      │        │          │      │
+└──────────┴──────┘        └──────────┴──────┘        └──────────┴──────┘        └──────────┴──────┘
+ Data       Next(Reference link)
+
+Basically a linked list consists of nodes where each node contains a data field and a reference(link) to the next node in the list.
+
+Types of Linked list:
+Singly Linked List: Navigation is forward only.
+Doubly Linked List: Forward and backward navigation is possible.
+Circular Linked List: Last element is linked to the first element.
+
+
+*/
+
+#include<iostream>
+using namespace std;
+int main() {
+
+
+
+
+
+    return 0;
+}
+
---
+++ Popular_questions/a_sword_puzzle.cpp
@@ -0,0 +1,31 @@
+/*   					8		1
+										
+					7				2
+
+					6				3
+
+						5		4		  
+
+
+Initially 8 people are standing in circular form and person1 is given sword 
+and he kills the next person and gives the sword to the next and it goes on.
+Implement a program to find the last one remaining.
+
+*/
+
+
+#include<iostream>
+using namespace std;
+int main() {
+		int n;
+		cout << "Enter the numbers" << endl;
+		int arr[n];
+		cin >> n;
+
+
+
+
+
+
+	return 0;
+}
